982a497c9dcfcf15cc54e3a998a3eaf4397cff80
InsertOwO <insertowo@noreply.codeberg.org>
2025-05-02 16:18:38 +0200

    mewo

--- modu/wm/wm.nix
+++ modu/wm/wm.nix
@@ -3,12 +3,11 @@
 {
   imports = [
     ./hypr.nix
-#    ./sway.nix
+    ./sway.nix
     ./rofi.nix
     ./wlogout.nix
     ./waybar.nix
     ./mako.nix
-    ./wob.nix
     ../apps/foot.nix
   ];
 }
